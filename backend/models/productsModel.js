230ae01270bac83a8c733f3ad984c90cd8ccfffa
Achal Vats <achalvats15@gmail.com>
2023-12-12 03:25:18 -0500

    Fix typo in numReviews property name

--- backend/models/productsModel.js
+++ backend/models/productsModel.js
@@ -54,7 +54,7 @@ const productSchema = new mongoose.Schema({
         required: true,
         default: 0
     },
-    numReviws: {
+    numReviews: {
         type: Number,
         required: true,
         default: 0
