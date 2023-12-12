ab71cadf436e59dd4e25b10eb6a49b8a08e8f1fd
Achal Vats <achalvats15@gmail.com>
2023-12-12 03:24:59 -0500

    Commented out HomeScreen import and unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap"
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import HomeScreen from "./screens/HomeScreen"
+// import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
@@ -11,8 +11,6 @@ const App = () => {
       <Header />
         <Container>
           <Outlet />
-          {/* <h1>Welcome to ProShop</h1> */}
-          {/* <HomeScreen /> */}
          </Container>
       </main>
       <Footer />
