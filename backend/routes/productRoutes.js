807bc59fbeae4ea8ca7b54c368152ce22e0673b5
Achal Vats <achalvats15@gmail.com>
2023-12-12 03:25:57 -0500

    Add error handling for product not found

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,7 +13,16 @@ router.get('/', asyncHandler(async (req, res) => {
 
 router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
-    res.json(product)
+
+    if (product) {
+        return res.json(product)
+    }
+    else{
+        res.status(404)
+        throw new Error('Resource not found')
+    }
+
+    
 }))
 
 export default router
