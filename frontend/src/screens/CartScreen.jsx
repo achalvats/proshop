9a36b1291b10720fa366d664592324800d6850b7
Achal Vats <achalvats15@gmail.com>
2023-12-25 20:01:49 -0500

    Add cart items to CartScreen image

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -20,7 +20,17 @@ const CartScreen = () => {
                     Your Cart is empty <Link to = '/'>Go Back</Link>
                 </Message>
             ) : (
-                <ListGroup variant='flush'>Items</ListGroup>
+                <ListGroup variant='flush'>
+                    {cartItems.map((item) => (
+                        <ListGroup.Item key={item._id}>
+                            <Row>
+                                <Col md ={2}>
+                                    <Image src={item.image} alt={item.name} fluid rounded />
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
             ) }
         </Col>
     </Row>
