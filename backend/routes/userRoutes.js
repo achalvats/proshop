c4afd267688e1d3e7a99ad4430e9acf1e2a39f11
Achal Vats <achalvats15@gmail.com>
2024-01-05 19:20:31 -0500

    Update login route to auth route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -17,7 +17,7 @@ const router = express.Router();
 
 router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
-router.post('/login', authUser);
+router.post('/auth', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 router.route('/:id').delete(protect, admin, deleteUserByID).get(protect, admin, getUserByID).put(protect, admin, updateUserByID)
 
