15c43a22076c5011c7bef4e907bcf78c7bc64868
Achal Vats <achalvats15@gmail.com>
2024-01-08 20:37:11 -0500

    Add token generation for authenticated users and registered users

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -12,6 +12,7 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
+
         generateToken(res, user._id)
         
 
@@ -49,6 +50,8 @@ const registerUser = asyncHandler(async (req, res) => {
     });
 
     if (user) {
+        generateToken(res, user._id)
+        
         registerUser.status(201).json({
             _id: user._id,
             name: user.name,
