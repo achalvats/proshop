7ffbc9adfd6a2b0a1536d712a47ee22e0f9c11c8
Achal Vats <achalvats15@gmail.com>
2024-01-08 20:36:18 -0500

    Refactor userControllers.js to use generateToken function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from 'jsonwebtoken';
+import { generateToken } from "../utils/generateToken.js";
 
 
 // @desc Auth users and get products
@@ -12,20 +12,8 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
-        // create JWT token of session authentication
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '30d'
-        });
-
-        // Set JWT as HTTP-Only cookie
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 *60 *1000 // 30 Days
-        })
-
-
+        generateToken(res, user._id)
+        
 
         res.json({
             _id: user._id,
