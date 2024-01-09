145b5aa4affb3517b36bd7b07550850e30557c61
Achal Vats <achalvats15@gmail.com>
2024-01-08 20:51:01 -0500

    Refactor userControllers.js to return user profile and handle user not found error

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
         generateToken(res, user._id)
         
 
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -80,7 +80,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile')
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc Update user profile
