5f3053a2c66a0cdd4e11fdc7eb3165fcccee6806
Achal Vats <achalvats15@gmail.com>
2024-01-22 20:50:49 -0500

    Add logout functionality and API call

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from 'react-router-dom'
 import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout } from '../slices/authSlice'
+import { useLogoutMutation } from  '../slices/usersApiSlice'
 import logo from '../assets/logo.png'
 
 
@@ -9,8 +12,20 @@ const Header = () => {
   const { cartItems } = useSelector((state) => state.cart)
   const { userInfo } = useSelector((state) => state.auth)
 
-  const logoutHandler = () => {
-    console.log('logout');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login')
+    } catch (err) {
+      console.log(err);
+      
+    }
   }
 
   console.log(cartItems);
