e2377b143bb12b3db1fc5a0a7f70a43d6db69a12
Achal Vats <achalvats15@gmail.com>
2024-01-22 20:58:43 -0500

    Refactor usersApiSlice.js to include a new logout mutation

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -9,14 +9,15 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 method: 'POST',
                 body: data,
             }),
+        }),
         logout: builder.mutation({
             query: () => ({
                 url: `${USERS_URL}/logout`,
                 method: 'POST',
-            })
-        })
+            
+            }),
         }),
-})
-})
+    }),
+});
 
 export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
